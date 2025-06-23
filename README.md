eb9bd3c232bcc5f532da0882b926d368c4f6b73e
joe-vie <marcemg101@gmail.com>
2025-06-23 11:36:29 -0400

    workshop progress; will come to this soon

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # backend-workshop
+
+This is one of my first introductions to backend where we are learning about Node.js, MongoDB, and Postman
+
+I'm super excited to see what I will continue to do in the future with this workshop!
---
+++ backend/src/config/index.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+import connectDB from "./config/database.js";
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: "./.env",
+});
+
+const startServer = async () => {};
+try {
+} catch (err) {}
